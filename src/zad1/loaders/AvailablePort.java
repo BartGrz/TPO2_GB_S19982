04163ebee50b3a878645e9d6dcd133a89d2553c8
Bartek <bartekgrzesiuk@gmail.com>
2021-03-30 18:14:57 +0200

    added method to find available port

---
+++ src/zad1/loaders/AvailablePort.java
@@ -0,0 +1,31 @@
+package zad1.loaders;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class AvailablePort {
+
+    public static int findFreePort() {
+        ServerSocket socket = null;
+        try {
+            socket = new ServerSocket(0);
+            socket.setReuseAddress(true);
+            int port = socket.getLocalPort();
+            try {
+                socket.close();
+            } catch (IOException e) {
+                // Ignore IOException on close()
+            }
+            return port;
+        } catch (IOException e) {
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+        throw new IllegalStateException("Could not find a free TCP/IP port to start embedded Jetty HTTP Server on");
+    }
+}
